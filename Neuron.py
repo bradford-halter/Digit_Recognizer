3ace475a51d1075b728f88d2753ea9349b200cce
Bradford Halter <bradfordhalter@hotmail.com>
2020-09-16 22:12:03 -0700

    Update Neuron.py

--- Neuron.py
+++ Neuron.py
@@ -1,1 +1,11 @@
+#Neuron class
+#
+#This contains a bias, an alpha value, and a set of weights based on the number of nodes in the previous layer.
 
+class neuron:
+   def __init__(self, num_weights):
+      self.bias = 0
+      self.alpha = 0
+      self.weights = []
+      for x in range(num_weights):
+         self.weights.append(0)
