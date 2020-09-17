ffa333ce512ee37c70e0bc9c9188d1542aa8f0e2
Bradford Halter <bradfordhalter@hotmail.com>
2020-09-16 23:05:34 -0700

    Update layer.py

--- layer.py
+++ layer.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-from neuron import Neuron
+from neuron import Neuron as n
 
 class Layer:
     def __init__(self, num_neurons, num_neurons_prev_layer):
         self.neurons = []
         for x in range(num_neurons):
-            self.neurons.append(neuron(num_neurons_prev_layer))
+            self.neurons.append(n(num_neurons_prev_layer))
     
     def __str__(self):
         return json.dumps(self.__dict__, separators=(',', ': '), sort_keys=True)
