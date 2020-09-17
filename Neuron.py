5ec2b6824cec45b4045a5ac67f31c3574d05be9a
Bradford Halter <bradfordhalter@hotmail.com>
2020-09-16 22:14:20 -0700

    Update Neuron.py

--- Neuron.py
+++ Neuron.py
@@ -4,8 +4,8 @@
 
 class neuron:
    def __init__(self, num_weights):
-      self.bias = 0
       self.alpha = 0
+      self.bias = 0
       self.weights = []
       for x in range(num_weights):
          self.weights.append(0)
