b7af81afa4fc9a053ac14f66dd03d22822334056
Bradford Halter <bradfordhalter@hotmail.com>
2020-09-16 22:55:59 -0700

    Update layer.py

--- layer.py
+++ layer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
-from . import Neuron
+from . import neuron
 
-
+class layer:
+   def __init__(self, num_neurons, num_neurons_prev_layer):
+      self.neurons = []
+      for x in range(num_neurons):
+         self.neurons.append(Neuron(num_neurons_prev_layer))
