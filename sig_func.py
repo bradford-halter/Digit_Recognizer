2c5b1542f7a602f4abb26c277a2cb478d6e21591
Bradford Halter <bradfordhalter@hotmail.com>
2020-09-24 21:03:35 -0700

    Update sig_func.py

--- sig_func.py
+++ sig_func.py
@@ -6,8 +6,8 @@ def sig_func(x):
 def cross_prod(active_neuron, prev_layer):
     ret_val = 0
     
-    for x in range(prev_layer):
-        ret_val += active_neuron.weights[x] * prev_layer.neurons[x].alpha
+    for i in range(prev_layer):
+        ret_val += active_neuron.weights[i] * prev_layer.neurons[i].alpha
         
     ret_val = sig_func(ret_val + bias)
     
