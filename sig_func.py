251315d408a2ea85b272989d5976b9494f934bdb
Bradford Halter <bradfordhalter@hotmail.com>
2020-09-24 23:00:19 -0700

    Update sig_func.py

--- sig_func.py
+++ sig_func.py
@@ -10,7 +10,7 @@ def sigmoid_function(x):
 def sig_func(active_neuron, prev_layer):
     ret_val = 0
     
-    for i in range(len(prev_layer)):
+    for i in range(len(prev_layer.neurons)):
         ret_val += active_neuron.weights[i] * prev_layer.neurons[i].alpha
         
     ret_val = sigmoid_function(ret_val + bias)
