3d76f2b6af7cde9e7a8709eb9298071b9e6a6173
Bradford Halter <bradfordhalter@hotmail.com>
2020-09-25 21:40:43 -0700

    Update network.py

--- network.py
+++ network.py
@@ -11,7 +11,7 @@ class Network:
         if num_hidden_layers > 1:
             self.hidden_layers = []
             self.hidden_layers.append(Layer(num_neurons_per_layer,num_test_feat))
-            for i in range(num_hidden_layers-1):
+            for i in range(num_hidden_layers-2):
                 self.hidden_layers.append(Layer(num_neurons_per_layer,num_neurons_per_layer))
             self.hidden_layers.append(Layer(num_possible_outputs, num_neurons_per_layer))
         elif num_hidden_layers == 1:
