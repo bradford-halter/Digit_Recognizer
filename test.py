46e292877b779cd839f58df11375bef291b445d6
Bradford Halter <bradfordhalter@hotmail.com>
2020-09-25 17:24:59 -0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,22 @@
+#I'm using this to test functions
+
+from network import Network
+from layer import Layer
+from neuron import Neuron
+
+from sig_func import *
+
+import jsonpickle
+
+def test1():
+    test_neuron = Neuron(2)
+    test_layer = Layer(1, 2)
+    test_layer.neurons[0] = 0
+    test_layer.neurons[1] = 1
+    test_neuron.weights[0] = 1
+    test_neuron.weights[1] = 1
+    
+    print(sig_func(test_neuron, test_layer))
+
+def main():
+    test1()
