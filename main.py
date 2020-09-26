4d7252fef0a8def0b66823297dbca171bb52b9ff
Daniel Mamaghani <daniel.mamaghani@gmail.com>
2020-09-26 01:40:59 -0700

    add initialize_network_for_testing() to set weights to 1 and bias to 0.

--- main.py
+++ main.py
@@ -16,6 +16,15 @@ def test_exp_1():
     print("test_exp_1()")
     print(math.exp(1))
  
+# For testing: Change the weights in each neuron to 1, and the bias to 0.
+def initialize_network_for_testing(network, initial_weight = 1, initial_bias = 0):
+    for cur_layer in network.hidden_layers:
+        for cur_neuron in cur_layer.neurons:
+            cur_neuron.bias = initial_bias
+            for i in range(len(cur_neuron.weights)):
+                cur_neuron.weights[i] = initial_weight
+    
+ 
 def test_cross_prod_1():
     '''
     # of hidden layer = 1
@@ -32,14 +41,16 @@ def test_cross_prod_1():
     Output: All of these neurons should be exactly the same, 
     and the output should be 0.73.
     
-    Note: To run this test, the weights in each Neuron (in neuron.py) must default to a value of 1.
+    Note: To run this test, the weights in each Neuron (in neuron.py) must default to a value of 1, and bias must default to 0.
     '''
     
     print("Started test_cross_prod_1()")
     
     alpha_values_for_input_layer = [0.0, 0.25, 0.5, 0.25]
     
-    my_network = Network(3, 4, 3, 3)
+    my_network = Network(1, 4, 3, 3)
+    
+    initialize_network_for_testing(my_network)
     
     # Create the input layer
     input_layer = Layer(4, 1)
@@ -53,7 +64,6 @@ def test_cross_prod_1():
     alpha_values_for_output_layer = propagation(my_network, input_layer)
     
     classification_values = classify_propagation_output(my_network)
-    
     
     print()
     print("Output layer alpha values:")
@@ -71,9 +81,11 @@ def test_cross_prod_1():
     # All alpha values should be 0.73
     for alpha_val in alpha_values_for_output_layer:
         if abs(alpha_val - 0.73) > 0.01:
-            return "Test failed :)"
+            print("Test failed :)")
+            return
     
-    return "Test succeeded! Amazing!"
+    print("Test succeeded! Amazing!")
+    return
     
  
 def main():
@@ -96,9 +108,9 @@ def main():
             #print(row)
             # Let's be real, we don't want to print this much stuff.
             break
-            
-        print(test_cross_prod_1())
-        test_exp_1()
+                      
+        test_cross_prod_1()
+        #test_exp_1()
             
         # Exit the program.
         print("Done.")
