2eeab99ec3f50df6498ac2ff20b5f3368a2ebfd1
Bradford Halter <bradfordhalter@hotmail.com>
2020-09-30 22:48:31 -0700

    Update cost_func.py

--- cost_func.py
+++ cost_func.py
@@ -4,8 +4,10 @@ import math
 
 def cost_func(output_layer_alphas, desired_alphas):
     ret_val = 0
+    ret_val_array = []
     
     for i in range(len(output_layer_alphas)):
         ret_val += ((output_layer_alphas[i] - desired_alphas[i]) ** 2)
+        ret_val_array[i].append((output_layer_alphas[i] - desired_alphas[i]) ** 2)
         
-    return ret_val
+    return [ret_val ret_val_array]
